dc2b2f96af0a0d147518273cda6cf2db001a6e59
Ult1mateXPHP <alex.ultimatex@gmail.com>
2024-12-17 13:52:55 +0300

    Documents WIP

---
+++ app/Http/Controllers/Play/Minecraft/DocumentController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Controllers\Play\Minecraft;
+
+use App\Http\Controllers\Controller;
+
+class DocumentController extends Controller
+{
+    public function index()
+    {
+
+    }
+}
