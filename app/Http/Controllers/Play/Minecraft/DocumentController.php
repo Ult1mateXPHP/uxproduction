89097c6ba75835fcfede4cc8d2ac052d83799467
Ult1mateXPHP <alex.ultimatex@gmail.com>
2024-12-17 13:55:23 +0300

    Documents WIP

--- app/Http/Controllers/Play/Minecraft/DocumentController.php
+++ app/Http/Controllers/Play/Minecraft/DocumentController.php
@@ -3,11 +3,24 @@
 namespace Controllers\Play\Minecraft;
 
 use App\Http\Controllers\Controller;
+use Domain\Play\Minecraft\Entity\DocumentEntity;
 
 class DocumentController extends Controller
 {
-    public function index()
+    public function get(int $user)
     {
+        return response()->json(DocumentEntity::query()->where('user_id', $user)->get());
+    }
 
+    public function create() {
+        //
+    }
+
+    public function edit() {
+        //
+    }
+
+    public function delete() {
+        //
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,8 @@ class User extends Authenticatable
         'name',
         //'email',
         'password',
-        'telegram_id'
+        'telegram_id',
+        'is_admin'
     ];
 
     /**
